b2823b4a19ed7b52ce1aafe3ba3721794847d2df
yeopseung <kwak991021@gmail.com>
2023-05-08 00:21:31 +0900

    feat: MakeTeamActivity3 모임 생성 버튼 기능 구현

--- app/src/main/java/com/example/moing/team/MakeTeamActivity3.java
+++ app/src/main/java/com/example/moing/team/MakeTeamActivity3.java
@@ -42,9 +42,14 @@ import java.io.File;
 
 public class MakeTeamActivity3 extends AppCompatActivity {
     private static final int READ_EXTERNAL_STORAGE_REQUEST = 0;
+    private EditText etIntroduce;
+    private EditText etResolution;
     private Button btnImageUpload;
     private Button btnImageUploadRe;
     private ImageView ivImageUpload;
+    private Button btnCreateTeam;
+    private ImageView ivProgressBar;
+
 
 
     @Override
@@ -60,14 +65,18 @@ public class MakeTeamActivity3 extends AppCompatActivity {
         btnBack.setOnClickListener(view -> finish());
 
         // 소모임 소개
-        EditText etIntroduce = findViewById(R.id.make_team3_et_introduce);
+        etIntroduce = findViewById(R.id.make_team3_et_introduce);
         TextView tvIntroduce = findViewById(R.id.make_team3_tv_introduce);
         TextView tvIntroduceCnt = findViewById(R.id.make_team3_tv_introduce_count);
 
         // 소모임장 각오
-        EditText etResolution = findViewById(R.id.make_team3_et_resolution);
+        etResolution = findViewById(R.id.make_team3_et_resolution);
         TextView tvResolution = findViewById(R.id.make_team3_tv_resolution);
         TextView tvResolutionCnt = findViewById(R.id.make_team3_tv_resolution_count);
+
+        // 소모임 생성 및 진행 상황
+        btnCreateTeam = findViewById(R.id.make_team3_btn_create_team);
+        ivProgressBar = findViewById(R.id.make_team3_iv_progress);
 
         // 소모임 사진 업로드
         btnImageUpload = findViewById(R.id.make_team3_btn_image_upload);
@@ -75,22 +84,33 @@ public class MakeTeamActivity3 extends AppCompatActivity {
         ivImageUpload = findViewById(R.id.make_team3_iv_image_upload);
 
         // onFocusChangeListener, TextWatcher 등록
-        setFocus(etIntroduce, tvIntroduce);
+        setFocus(etIntroduce, tvIntroduce, tvIntroduceCnt);
         setTextWatcher(etIntroduce, tvIntroduceCnt, 300);
-        setFocus(etResolution, tvResolution);
+        setFocus(etResolution, tvResolution, tvResolutionCnt);
         setTextWatcher(etResolution, tvResolutionCnt, 100);
 
         // onClickListener 등록
         btnImageUpload.setOnClickListener(onImageUploadClickListener);
         btnImageUploadRe.setOnClickListener(onImageUploadClickListener);
+        btnCreateTeam.setOnClickListener(onCreateTeamClickListener);
 
-
+        // 모임 생성 버튼 비활성화
+        btnCreateTeam.setClickable(false);
 
         // 구현 예정 - S3를 이용한 사진 업로드
         // 구현 예정 - 생성하기 버튼 및 ProgressBar
     }
 
-    // 버튼 클릭 시 갤러리 열기
+    /** 버튼 클릭 시 소모임 Data 전송 후 다음 화면 실행 **/
+    View.OnClickListener onCreateTeamClickListener = view -> {
+        // 구현 예정 - 소모임 Data Retrofit 사용하여 전달
+
+        // 다음 화면
+        Intent intent = new Intent(getApplicationContext(), MakeTeamActivity4.class);
+        startActivity(intent);
+    };
+
+    /** 버튼 클릭 시 권한 확인 후 갤러리 접근 **/
     View.OnClickListener onImageUploadClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -108,7 +128,7 @@ public class MakeTeamActivity3 extends AppCompatActivity {
         }
     };
 
-    // 갤러리에서 이미지 선택을 위한 ActivityResultLauncher 생성
+    /** 갤러리에서 이미지 선택을 위한 ActivityResultLauncher **/
     ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -127,6 +147,8 @@ public class MakeTeamActivity3 extends AppCompatActivity {
                         btnImageUpload.setVisibility(View.GONE);
                         ivImageUpload.setVisibility(View.VISIBLE);
                         btnImageUploadRe.setVisibility(View.VISIBLE);
+
+                        checkInputs();
                     }
                 }
             }
@@ -134,9 +156,17 @@ public class MakeTeamActivity3 extends AppCompatActivity {
 
 
     /** EditText focus 시 제목 색상 변경 **/
-    private void setFocus(EditText editText, TextView textView) {
-        editText.setOnFocusChangeListener((view, isFocused) ->
-                textView.setTextColor(isFocused ? getResources().getColor(R.color.main_dark_200) : getResources().getColor(R.color.secondary_grey_black_7)));
+    private void setFocus(EditText editText, TextView textView, TextView textViewCnt) {
+        editText.setOnFocusChangeListener((view, isFocused) -> {
+            if(isFocused){
+                textView.setTextColor(getResources().getColor(R.color.main_dark_200));
+                textViewCnt.setTextColor(getResources().getColor(R.color.main_dark_200));
+            }
+            else{
+                textView.setTextColor(getResources().getColor(R.color.secondary_grey_black_7));
+                textViewCnt.setTextColor(getResources().getColor(R.color.secondary_grey_black_7));
+            }
+        });
     }
 
     /** EditText 입력 시 입력된 글자 수 변경 **/
@@ -152,19 +182,36 @@ public class MakeTeamActivity3 extends AppCompatActivity {
             public void afterTextChanged(Editable editable) {
                 String result = "(" + editable.length() + "/" + maxLength + ")";
                 countTextView.setText(result);
+                checkInputs();
             }
         });
     }
 
+    /** 소모임 소개, 각오, 사진이 올바르게 입력되었는지 확인 후 버튼 및 ProgressBar 변경 **/
+    private void checkInputs(){
+        if(etIntroduce.length() > 0 && etResolution.length() > 0 && ivImageUpload.getVisibility() == View.VISIBLE) {
+            btnCreateTeam.setBackgroundResource(R.drawable.button_round_black1);
+            btnCreateTeam.setTextColor(ContextCompat.getColorStateList(getApplicationContext(), R.color.secondary_grey_black_12));
+            btnCreateTeam.setClickable(true);
+            ivProgressBar.setBackgroundResource(R.drawable.maketeam_progress5);
+        }
+        else{
+            btnCreateTeam.setTextColor(ContextCompat.getColorStateList(getApplicationContext(), R.color.secondary_grey_black_10));
+            btnCreateTeam.setBackgroundResource(R.drawable.button_round_black12);
+            btnCreateTeam.setClickable(false);
+            ivProgressBar.setBackgroundResource(R.drawable.maketeam_progress4);
+        }
+    }
+
     public void uploadWithTransferUtility(String fileName, File file) {
 
-        AWSCredentials awsCredentials = new BasicAWSCredentials("AKIA5L3BRFIIWLIJ62JQ", "xywH1VJjcAxp0xcEyFuVrOnknLp3XtWsyT2KaFEx");    // IAM 생성하며 받은 것 입력
+        AWSCredentials awsCredentials = new BasicAWSCredentials("AKA5L3BROCCOLIJS62JQ", "xywH1VJjcAxp0xcEyFuVrOnknLp3XtWsyT2KaFEx");    // IAM 생성하며 받은 것 입력
         AmazonS3Client s3Client = new AmazonS3Client(awsCredentials, Region.getRegion(Regions.AP_NORTHEAST_2));
 
         TransferUtility transferUtility = TransferUtility.builder().s3Client(s3Client).context(getApplicationContext()).build();
         TransferNetworkLossHandler.getInstance(getApplicationContext());
 
-        TransferObserver uploadObserver = transferUtility.upload("moing-images", fileName, file);    // (bucket api, file이름, file객체)
+        TransferObserver uploadObserver = transferUtility.upload("moing-images", fileName, file);    // (bucket api, file, file)
 
         uploadObserver.setTransferListener(new TransferListener() {
             @Override
@@ -177,12 +224,12 @@ public class MakeTeamActivity3 extends AppCompatActivity {
             @Override
             public void onProgressChanged(int id, long current, long total) {
                 int done = (int) (((double) current / total) * 100.0);
-                Log.d("MYTAG", "UPLOAD - - ID: $id, percent done = $done");
+                Log.d("MY-TAG", "UPLOAD - - ID: $id, percent done = $done");
             }
 
             @Override
             public void onError(int id, Exception ex) {
-                Log.d("MYTAG", "UPLOAD ERROR - - ID: $id - - EX:" + ex.toString());
+                Log.d("MY-TAG", "UPLOAD ERROR - - ID: $id - - EX:" + ex.toString());
             }
         });
     }
