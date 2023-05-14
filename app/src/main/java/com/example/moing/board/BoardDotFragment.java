e9486e3d424e9a34a01eb531e140616396ba05d1
yeopseung <kwak991021@gmail.com>
2023-05-14 15:53:55 +0900

    feat: 소모임 초대링크 복사하기 기능 구현 완료

--- app/src/main/java/com/example/moing/board/BoardDotFragment.java
+++ app/src/main/java/com/example/moing/board/BoardDotFragment.java
@@ -1,43 +1,44 @@
 package com.example.moing.board;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.example.moing.R;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BoardDotFragment extends BottomSheetDialogFragment {
 
-    ImageButton link, fix, close;
-
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_board_dot, container, false);
 
-        link = view.findViewById(R.id.imgbtn_link);
-        fix = view.findViewById(R.id.imgbtn_fix);
-        close = view.findViewById(R.id.imgbtn_close);
+        ImageButton btnLink = view.findViewById(R.id.board_dot_btn_link);
+        ImageButton btnFix = view.findViewById(R.id.board_dot_btn_fix);
+        ImageButton btnClose = view.findViewById(R.id.board_dot_btn_close);
 
-        close.setOnClickListener(closeClickListener);
-
-
+        btnLink.setOnClickListener(linkClickListener);
+        btnClose.setOnClickListener(closeClickListener);
 
         return view;
     }
+    View.OnClickListener linkClickListener = v ->{
+        BoardInviteCodePopup customPopup = new BoardInviteCodePopup(requireContext());
+        // 구현 예정 - 초대 코드를 서버로 부터 받아와 설정
+        customPopup.setInviteCode("12345");
+        // 팝업의 위치와 크기를 설정 하여 표시
+        customPopup.showAtLocation(v, Gravity.CENTER, 0, 0);
+        // BoardDotFragment 도 닫음
+        customPopup.setOnDismissListener(this::dismiss);
+    };
 
     // 뒤로 가기
-    View.OnClickListener closeClickListener = v -> {
-      dismiss();
-    };
+    View.OnClickListener closeClickListener = v -> dismiss();
 }
