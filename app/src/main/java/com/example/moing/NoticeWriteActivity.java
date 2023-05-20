78f5df5f396118b8bcb54bd518cb1329d5919f39
dihyeonee <xnaxna1@naver.com>
2023-05-20 14:18:51 +0900

    feat : 공지사항 작성하기 추가

---
+++ app/src/main/java/com/example/moing/NoticeWriteActivity.java
@@ -0,0 +1,14 @@
+package com.example.moing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoticeWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice_write);
+    }
+}
