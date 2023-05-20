8ba56d210a7472e05361b86e67ad4af31dbe56cb
yeopseung <kwak991021@gmail.com>
2023-05-20 18:29:57 +0900

    feat: 미션 인증 현황 (인증 완료) 레이아웃 구현 완료

--- app/src/main/java/com/example/moing/mission/MissionDoneAdapter.java
+++ app/src/main/java/com/example/moing/mission/MissionDoneAdapter.java
@@ -1,6 +1,7 @@
 package com.example.moing.mission;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,15 +13,30 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moing.R;
 import com.example.moing.team.Team;
+import com.example.moing.team.TeamAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MissionDoneAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
-    ArrayList<Mission> missions;
+    private static final int DONE_TYPE = 0;
+    private static final int PENDING_TYPE = 1;
+    List<Mission> missionList;
 
-    public MissionDoneAdapter(ArrayList<Mission> missions) {
-        this.missions = missions;
+    public MissionDoneAdapter(List<Mission> missionList) {
+        this.missionList = missionList;
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        // 마지막 DefaultType
+        if (missionList.get(position).status.equals("COMPELTE")) {
+            return DONE_TYPE;
+        }
+        // 그 외 TeamType
+        return PENDING_TYPE;
+    }
+
 
     @NonNull
     @Override
@@ -30,37 +46,83 @@ public class MissionDoneAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         Context context = parent.getContext();
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        view = inflater.inflate(R.layout.recycler_item_team_default,parent,false);
-        viewHolder = new MissionDoneAdapter.DoneViewHolder(view);
+        // viewType 에 따라 ViewHolder 와 Layout 설정
+        if(viewType == DONE_TYPE){
+            // 완료 상태
+            view = inflater.inflate(R.layout.recycler_item_mission_done,parent,false);
+            viewHolder = new MissionDoneAdapter.DoneViewHolder(view);
+        }
+        else{
+            // 건너뛰기 상태
+            view = inflater.inflate(R.layout.recycler_item_mission_pending,parent,false);
+            viewHolder = new MissionDoneAdapter.PendingViewHolder(view);
+        }
 
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-       DoneViewHolder doneViewHolder = (DoneViewHolder) holder;
-       doneViewHolder.tvNickname.setText("hi");
+        // viewType 에 따라 text 및 image 설정
+        Mission mission = missionList.get(position);
+
+        switch (holder.getItemViewType()) {
+            case DONE_TYPE:
+                MissionDoneAdapter.DoneViewHolder doneViewHolder = (DoneViewHolder) holder;
+
+                // 첫번째는 나 표시
+                if(position == 0)
+                    doneViewHolder.ivMe.setVisibility(View.VISIBLE);
+
+                // 완료에 대한 리사이클러뷰들을 받아와 처리
+
+
+                break;
+            case PENDING_TYPE:
+                MissionDoneAdapter.PendingViewHolder pendingViewHolder = (PendingViewHolder) holder;
+
+                // 첫번째는 나 표시
+                if(position == 0)
+                    pendingViewHolder.ivMe.setVisibility(View.VISIBLE);
+
+                // 건너뛰기에 대한 리사이클러뷰들을 받아와 처리
+
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return missionList.size();
     }
 
-    public static class DoneViewHolder extends RecyclerView.ViewHolder {
+    class DoneViewHolder extends RecyclerView.ViewHolder{
         ImageView ivArchive;
         ImageView ivProfile;
+        ImageView ivMe;
         TextView tvNickname;
-
 
         public DoneViewHolder(@NonNull View itemView) {
             super(itemView);
             ivArchive = itemView.findViewById(R.id.mission_done_iv_archive);
             ivProfile = itemView.findViewById(R.id.mission_done_iv_profile);
+            ivMe = itemView.findViewById(R.id.mission_done_iv_me);
             tvNickname = itemView.findViewById(R.id.mission_done_tv_nickname);
+        }
+    }
 
-            // S3 -> Glide를 통한 이미지 설정
+    class PendingViewHolder extends RecyclerView.ViewHolder{
+        ImageView ivProfile;
+        ImageView ivMe;
+        TextView tvReason;
+        TextView tvNickname;
 
+        public PendingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProfile = itemView.findViewById(R.id.mission_pending_iv_profile);
+            ivMe = itemView.findViewById(R.id.mission_pending_iv_me);
+            tvReason = itemView.findViewById(R.id.mission_pending_tv_reason);
+            tvNickname = itemView.findViewById(R.id.mission_pending_tv_nickname);
 
         }
     }
--- app/src/main/java/com/example/moing/mission/MissionStatusDoneFragment.java
+++ app/src/main/java/com/example/moing/mission/MissionStatusDoneFragment.java
@@ -1,18 +1,18 @@
 package com.example.moing.mission;
 
+import android.graphics.Rect;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moing.R;
-import com.example.moing.team.Team;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,12 +23,15 @@ public class MissionStatusDoneFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mission_status_done, container, false);
 
-        ArrayList<Mission> missions = new ArrayList<>();
-        missions.add(new Mission(1,"1","img","COMPLETE","archive","submitDate"));
-        missions.add(new Mission(2,"1","img","PENDING","archive","submitDate"));
-        missions.add(new Mission(3,"1","img","COMPLETE","archive","submitDate"));
-        missions.add(new Mission(4,"1","img","COMPLETE","archive","submitDate"));
-        missions.add(new Mission(1,"1","img","PENDING","archive","submitDate"));
+        List<Mission> missionList = new ArrayList<>();
+        missionList.add(new Mission(1,"1","img","COMPELTE","archive","submitDate"));
+        missionList.add(new Mission(2,"1","img","COMPELTE","archive","submitDate"));
+        missionList.add(new Mission(4,"1","img","PENDING","archive","submitDate"));
+        missionList.add(new Mission(1,"1","img","PENDING","archive","submitDate"));
+        missionList.add(new Mission(1,"1","img","COMPELTE","archive","submitDate"));
+        missionList.add(new Mission(2,"1","img","COMPELTE","archive","submitDate"));
+        missionList.add(new Mission(4,"1","img","PENDING","archive","submitDate"));
+        missionList.add(new Mission(1,"1","img","PENDING","archive","submitDate"));
 
         // 리사이클러뷰
         RecyclerView recyclerView = view.findViewById(R.id.mission_status_done_rv);
@@ -37,9 +40,21 @@ public class MissionStatusDoneFragment extends Fragment {
         GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
         recyclerView.setLayoutManager(layoutManager);
 
+        // 리사이클러뷰 아이템들 간 간격 설정
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.set(0, 24, 0, 24);
+            }
+        });
+
         // 리사이클러뷰에 사용할 어댑터 설정
-        MissionDoneAdapter adapter = new MissionDoneAdapter(missions);
+        MissionDoneAdapter adapter = new MissionDoneAdapter(missionList);
         recyclerView.setAdapter(adapter);
+
+        TextView tvNum = view.findViewById(R.id.mission_status_done_tv_num);
+        tvNum.setText(missionList.size()+"명");
 
 
         return view;
