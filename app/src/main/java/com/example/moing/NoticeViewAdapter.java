40811ca6ac515a1b3f860ba314bd406c7d840028
hyunseokSon <fairy0724@soongsil.ac.kr>
2023-05-22 01:18:34 +0900

    공지, 투표 연동(50%)

---
+++ app/src/main/java/com/example/moing/NoticeViewAdapter.java
@@ -0,0 +1,103 @@
+package com.example.moing;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.moing.Response.AllNoticeResponse;
+
+import java.util.List;
+
+public class NoticeViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private List<AllNoticeResponse.NoticeBlock> listData;
+    private Context context;
+
+    // 클릭 아이템 리스너
+    private OnItemClickListener onItemClickListener = null;
+
+    // 클릭 리스너 인터페이스
+    public interface OnItemClickListener {
+        void onItemClick(int pos);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+
+    public NoticeViewAdapter(List<AllNoticeResponse.NoticeBlock> listData, Context context) {
+        this.listData = listData;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.recycler_item_notice, parent, false);
+        return new NoticeViewAdapter.NoticeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        AllNoticeResponse.NoticeBlock item = listData.get(position);
+        NoticeViewHolder vh = (NoticeViewHolder) holder;
+
+        /** 이미지 빼고 연동했습니다. **/
+        vh.image.setBackgroundResource(R.drawable.notice_profile); // 닉네임
+        vh.name.setText(item.getNickName()); // 이름
+        vh.crown.setBackgroundResource(R.drawable.notice_crown); // 왕관
+        vh.title.setText(item.getTitle());
+        vh.content.setText(item.getContent());
+        vh.dot.setBackgroundResource(R.drawable.notice_dot);
+        vh.count.setText(item.getCommentNum());
+
+        boolean read = item.getRead();
+        vh.dot.setVisibility(read ? View.INVISIBLE : View.VISIBLE); // 참이면 안보이게, 거짓이면 보이게
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    public class NoticeViewHolder extends RecyclerView.ViewHolder {
+        ImageView image, dot, crown, iv_msg;
+        ConstraintLayout background;
+        TextView name, title, content, count;
+
+        public NoticeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            background = itemView.findViewById(R.id.recycle_background);
+            crown = itemView.findViewById(R.id.notice_iv_crown);
+            image = itemView.findViewById(R.id.notice_iv_user_image);
+            name = itemView.findViewById(R.id.notice_tv_user_name);
+            dot = itemView.findViewById(R.id.notice_iv_dot);
+            title = itemView.findViewById(R.id.notice_tv_title);
+            content = itemView.findViewById(R.id.notice_tv_content);
+            iv_msg = itemView.findViewById(R.id.notice_iv_message);
+            count = itemView.findViewById(R.id.comment_count);
+
+            // 아이템 클릭 이벤트 추가
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+                        // 리스너 객체 메서드 호출
+                        if(onItemClickListener != null) {
+                            onItemClickListener.onItemClick(pos);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/moing/board/BoardGoalAdapter.java
+++ app/src/main/java/com/example/moing/board/BoardGoalAdapter.java
@@ -9,13 +9,15 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.moing.R;
+import com.example.moing.Response.BoardNoReadNoticeResponse;
+import com.example.moing.Response.BoardNoReadVoteResponse;
 
 import java.util.List;
 
-public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.BoardViewHolder> {
-    private List<Board> boardList;
+public class BoardGoalAdapter<T> extends RecyclerView.Adapter<BoardGoalAdapter.BoardViewHolder> {
     private BoardGoalFragment context;
-    private int seeItem = 3; // 3개만 아이템을 보여주겠다!
+    private List<T> dataList;
+
 
     TextView boardTitle, boardContent;
     View boardLine;
@@ -31,14 +33,15 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.BoardViewHol
         this.onItemClickListener = listener;
     }
 
-    // 생성자
-    public BoardAdapter(List<Board> boardList, BoardGoalFragment context) {
-        this.boardList = boardList;
+    // 공지 생성자
+    public BoardGoalAdapter(List<T> dataList, BoardGoalFragment context) {
+        this.dataList = dataList;
         this.context = context;
     }
 
     class BoardViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
+
         public BoardViewHolder(@NonNull View itemView) {
             super(itemView);
             mView = itemView;
@@ -66,35 +69,37 @@ public class BoardAdapter extends RecyclerView.Adapter<BoardAdapter.BoardViewHol
 
     @NonNull
     @Override
-    public BoardAdapter.BoardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public BoardGoalAdapter.BoardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View view = layoutInflater.inflate(R.layout.recycler_item_board, parent, false);
         return new BoardViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull BoardAdapter.BoardViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull BoardGoalAdapter.BoardViewHolder holder, int position) {
         // 값을 가져오는 곳
+        T item = dataList.get(position);
 
-        // 3개까지만 출력하고 싶을 때
-        if (position < 3) {
-            // 제목
-           boardTitle.setText(boardList.get(position).getTitle());
-            // 내용
-            boardContent.setText(boardList.get(position).getContent());
-            // 3번째 내용을 출력할때는 Line을 제거한다.
+        if (item instanceof BoardNoReadNoticeResponse.NoticeData) {
+            BoardNoReadNoticeResponse.NoticeData data = (BoardNoReadNoticeResponse.NoticeData) item;
+            boardTitle.setText(data.getTitle());
+            boardContent.setText(data.getContent());
             if (position == 2) {
                 boardLine.setVisibility(View.GONE);
             }
         }
-
+        else if (item instanceof BoardNoReadVoteResponse.VoteData) {
+            BoardNoReadVoteResponse.VoteData data = (BoardNoReadVoteResponse.VoteData) item;
+            boardTitle.setText(data.getTitle());
+            boardContent.setText(data.getContent());
+            if (position == 2) {
+                boardLine.setVisibility(View.GONE);
+            }
+        }
     }
 
     @Override
     public int getItemCount() {
-        // 최대 3개만 보여주겠다!
-        return Math.min(boardList.size(), seeItem);
-        // 전체 다 반환하겠다!
-        // return boardList.size();
+        return dataList.size();
     }
 }
