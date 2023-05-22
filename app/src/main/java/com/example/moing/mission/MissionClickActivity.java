bd984ffb1421f45be1c1651830f1f9e21318b993
hyunseokSon <fairy0724@soongsil.ac.kr>
2023-05-22 09:45:15 +0900

    chore : 미션 인증하기 레이아웃(일부 수정)

--- app/src/main/java/com/example/moing/mission/MissionClickActivity.java
+++ app/src/main/java/com/example/moing/mission/MissionClickActivity.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
@@ -17,6 +18,7 @@ public class MissionClickActivity extends AppCompatActivity {
     private SubsamplingScaleImageView picture;
     private Dialog dialog;
     private ImageButton back_dialog;
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +27,15 @@ public class MissionClickActivity extends AppCompatActivity {
 
         // 사진 확대 기능
         picture = (SubsamplingScaleImageView) findViewById(R.id.mission_pic);
-        picture.setOnClickListener(pictureClickListener);
+
         /** 이미지 setting 예시 **/
 //        imageView.setImage(ImageSource.resource(R.drawable.monkey));
 //        imageView.setImage(ImageSource.asset("map.png"))
 //        imageView.setImage(ImageSource.uri("/sdcard/DCIM/DSCM00123.JPG"));
 
+        // 뒤로 가기
+        back = findViewById(R.id.btn_close);
+        // 사진 팝업 버튼
         dialog = new Dialog(MissionClickActivity.this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.mission_picture_dialog);
@@ -38,8 +43,14 @@ public class MissionClickActivity extends AppCompatActivity {
         back_dialog = dialog.findViewById(R.id.btn_back);
 
 
+        back.setOnClickListener(backClickListener);
+        picture.setOnClickListener(pictureClickListener);
+        // 다이얼로그 뒤로 가기 버튼
         back_dialog.setOnClickListener(dialogBackClickListener);
     }
+
+    // 뒤로 가기 버튼 클릭 시
+    View.OnClickListener backClickListener = v -> { finish();};
 
     // 사진 클릭 시
     View.OnClickListener pictureClickListener = v -> {
